3413b101f5646a896b672316ffbdc9cf7baf3f4c
Guneet Kaur <gungunk2003@gmail.com>
2024-07-28 21:23:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('https://icanhazdadjoke.com/slack')
     .then(data => data.json())
     .then(jokeData => {
         const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement'); // Corrected method name
+        const jokeElement = document.getElementById('jokeElement'); 
     
         jokeElement.innerHTML = jokeText;
     });
